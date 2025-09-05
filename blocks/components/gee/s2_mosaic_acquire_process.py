43214b42b4ed6289ab56796306e487fb6da28ba7
Datentreiber <benjamin.dilorenzo@datentreiber.de>
2025-09-05 17:50:31 +0200

    Update s2_mosaic_acquire_process.py

--- blocks/components/gee/s2_mosaic_acquire_process.py
+++ blocks/components/gee/s2_mosaic_acquire_process.py
@@ -34,13 +34,24 @@ Notes
 
 import ee
 
+def _mask_s2_sr(img: ee.Image) -> ee.Image:
+    qa = img.select("QA60")
+    cloud_bit, cirrus_bit = 1 << 10, 1 << 11
+    mask = qa.bitwiseAnd(cloud_bit).eq(0).And(qa.bitwiseAnd(cirrus_bit).eq(0))
+    return img.updateMask(mask).divide(10000).copyProperties(img, img.propertyNames())
+
 def build_s2_quarter_median(aoi: ee.Geometry, start: ee.Date, end: ee.Date) -> ee.Image:
     col = (
-        ee.ImageCollection("COPERNICUS/S2_HARMONIZED")
+        ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
         .filterDate(start, end)
         .filterBounds(aoi)
-        .filter(ee.Filter.lte("CLOUDY_PIXEL_PERCENTAGE", 10))
-        .map(lambda img: img.divide(10000))
+        .filter(ee.Filter.lte("CLOUDY_PIXEL_PERCENTAGE", 80))
+        .map(_mask_s2_sr)
     )
-    img = col.median().clip(aoi)
-    return img
+
+    size = col.size()
+    def _ok():
+        return (col.select(["B8","B11","B4"]).median().clip(aoi).set("empty", 0))
+    def _empty():
+        return (ee.Image.constant([0,0,0]).rename(["B8","B11","B4"]).toFloat().clip(aoi).set("empty", 1))
+    return ee.Image(ee.Algorithms.If(size.gt(0), _ok(), _empty()))
